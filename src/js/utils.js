bf7242ec29c29136fa9d4ed0561d020e04f60907
Anuki3008 <annakhalvashi7@gmail.com>
2025-07-19 02:57:32 +0400

    feat: add weekly weather

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,3 +8,24 @@ async function getCityFromCoordinates(latitude, longitude) {
 
   return data.address.city;
 }
+
+const getWeeklyWeatherData = async (place) => {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=9b1eafa8504d3df1a475ec2a4e57743f`;
+  const res = await fetch(apiUrl);
+
+  const weeklyWeather = await res.json();
+
+  const weeklyWeatherList = weeklyWeather.list;
+
+  const weatherObj = {};
+
+  weeklyWeatherList.forEach((item) => {
+    const date = new Date(item.dt_txt);
+
+    weatherObj[date.getDate()] = item;
+  });
+
+
+  return Object.values(weatherObj)
+};
+
